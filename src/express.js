538aff6a7972b04dbd6cc490a80058abc17a5f00
unknown <magedsopihy@gmail.com>
2021-05-31 16:38:07 +0200

    add awelcome message

--- src/express.js
+++ src/express.js
@@ -12,7 +12,7 @@ const productRoutes = require('./routes/product.routes')
 
 const CURRENT_WORKING_DIR = process.cwd()
 const app = express()
-
+app.use(cors())
 // parse body params and attache them to req.body
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
@@ -21,14 +21,6 @@ app.use(compress())
 // secure apps by setting various HTTP headers
 app.use(helmet())
 // enable CORS - Cross Origin Resource Sharing
-app.use(
-  cors({
-    origin: 'https://60b4a6342e9d1414e88eef53--nervous-boyd-fdc90b.netlify.app',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    preflightContinue: true,
-    optionsSuccessStatus: 204,
-  })
-)
 
 // mount routes
 app.use('/', userRoutes)
